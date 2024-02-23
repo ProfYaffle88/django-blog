e4597a8b8f1daa33c07de12b2ef2fe2916fdf758
James Price <james.a.t.price@gmail.com>
2024-02-23 14:07:22 +0000

    Make ready for deploy

--- codestar2024/settings.py
+++ codestar2024/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fi9otgaoeb8+tx8+4+p=qns-4)^m=e-*emmr$es7fxknp5luuj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-profyaffle88-djangoblog-a1zhi6vwp4x.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-profyaffle88-djangoblog-a1zhi6vwp4x.ws-eu108.gitpod.io','.herokuapp.com']
 
 
 # Application definition
